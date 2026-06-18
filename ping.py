54c02e1a959b4d02b4357a6f0a8c9054e8f0d9f2
federicotosca-bf <federico.tosca@brainfarm.eu>
2026-06-18 08:13:25 +0200

    Python supabase script

--- ping.py
+++ ping.py
@@ -1,3 +1,33 @@
 from pythonping import ping
 
 ping('https://sfmmavdtvgwvzpxpnvmy.supabase.co/rest/v1/', verbose=True)
+
+
+import os
+from flask import Flask
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+supabase: Client = create_client(
+    os.environ.get("SUPABASE_URL"),
+    os.environ.get("SUPABASE_PUBLISHABLE_KEY")
+)
+
+@app.route('/')
+def index():
+    response = supabase.table('instruments').select("*").execute()
+    instruments = response.data
+
+    html = '<h1>Instruments</h1><ul>'
+    for instrument in instruments:
+        html += f'<li>{instrument["name"]}</li>'
+    html += '</ul>'
+
+    return html
+
+if __name__ == '__main__':
+    app.run(debug=True)
